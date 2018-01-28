28c2f5656532ae19a9660988d66ca05ac6a9eedb
Ben Hall <Benjamin2442@hotmail.co.uk>
2018-01-28 20:15:04 +0000

    got second option working and clearing previous team selection

--- js/nav.js
+++ js/nav.js
@@ -19,22 +19,23 @@ $( document ).ready(function() {
 
 			$('#first-choice').change(function(event){
 				var selectedTeam = $('#first-choice').val();
-				console.log(selectedTeam);
+				// console.log(selectedTeam);
 				var teamData = data.teams.filter( a => a.name === selectedTeam);
-				console.log(teamData);
+				// console.log(teamData);
 				var teamCode = teamData[0].code;
-				console.log(teamCode);   //<<<<<<<<<<<<<<  THIS IS THE SELECTED TEAMS CODE USED TO FIND PLAYERS WITH MATCHING CODE
+				// console.log(teamCode);   //<<<<<<<<<<<<<<  THIS IS THE SELECTED TEAMS CODE USED TO FIND PLAYERS WITH MATCHING CODE
 
 				var playerList = data.elements.filter( a => a.team_code === teamCode);
-				console.log(playerList);
+				// console.log(playerList);  //<<<<<<<<<< THIS IS THE LIST OF PLAYERS IN THE SELECTED TEAM
 
 				playerHtml = playerList.map(function(v,i) {
 			    return '<option value="' + v.second_name + '">' + v.second_name + '</option>'
 				}).join(''); //<<<<<<<<< THIS CREATES EACH OPTION ITEM
 
-				$('#second-choice').append(playerHtml); //<<<<<<<<<< THIS ADDS THE OPTIONS TO OPTION ONE
+				$('#second-choice').empty().append(playerHtml); //<<<<<<<<<< THIS ADDS THE PLAYERS TO OPTION TWO
 
 			});
+
 
 	}); //<<<<< END OF VAR RESPONSE TO GET JSON
 }); //<<<<<< END OF DOCUMENT.READY
