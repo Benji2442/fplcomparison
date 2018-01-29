b6c8af27a8dba1bdac8202de86151c5ff3d4e64e
Ben Hall <Benjamin2442@hotmail.co.uk>
2018-01-29 15:46:31 +0000

    got both player dropdowns working independatly

--- js/nav.js
+++ js/nav.js
@@ -14,11 +14,11 @@ $( document ).ready(function() {
 		    return '<option class="listItem" value="' + v.name + '">' + v.name + '</option>'
 			}).join(''); //<<<<<<<<< THIS CREATES EACH OPTION ITEM
 
-			$('#first-choice').append(html); //<<<<<<<<<< THIS ADDS THE OPTIONS TO OPTION ONE
+			$('#first-choice1, #first-choice2').append(html); //<<<<<<<<<< THIS ADDS THE OPTIONS TO OPTION ONE
 
 
-			$('#first-choice').change(function(event){
-				var selectedTeam = $('#first-choice').val();
+			$('#first-choice1').change(function(event){
+				var selectedTeam = $('#first-choice1').val();
 				// console.log(selectedTeam);
 				var teamData = data.teams.filter( a => a.name === selectedTeam);
 				// console.log(teamData);
@@ -32,9 +32,32 @@ $( document ).ready(function() {
 			    return '<option class="listItem" value="' + v.second_name + '">' + v.second_name + '</option>'
 				}).join(''); //<<<<<<<<< THIS CREATES EACH OPTION ITEM
 
-				$('#second-choice').empty().append(playerHtml); //<<<<<<<<<< THIS ADDS THE PLAYERS TO OPTION TWO
+				$('#second-choice1').empty().append(playerHtml); //<<<<<<<<<< THIS ADDS THE PLAYERS TO OPTION TWO
 
-			});
+			}); //<<<<< END OF .CHANGE FUNCTION FOR PLAYER 1
+
+
+// THIS NEEDS CHANGING SO THAT IT ONLY USES ONE FUNCTION WITH PARAMETERS ADDED DYNAMICALLY
+
+
+			$('#first-choice2').change(function(event){
+				var selectedTeam = $('#first-choice2').val();
+				// console.log(selectedTeam);
+				var teamData = data.teams.filter( a => a.name === selectedTeam);
+				// console.log(teamData);
+				var teamCode = teamData[0].code;
+				// console.log(teamCode);   //<<<<<<<<<<<<<<  THIS IS THE SELECTED TEAMS CODE USED TO FIND PLAYERS WITH MATCHING CODE
+
+				var playerList = data.elements.filter( a => a.team_code === teamCode);
+				// console.log(playerList);  //<<<<<<<<<< THIS IS THE LIST OF PLAYERS IN THE SELECTED TEAM
+
+				playerHtml = playerList.map(function(v,i) {
+			    return '<option class="listItem" value="' + v.second_name + '">' + v.second_name + '</option>'
+				}).join(''); //<<<<<<<<< THIS CREATES EACH OPTION ITEM
+
+				$('#second-choice2').empty().append(playerHtml); //<<<<<<<<<< THIS ADDS THE PLAYERS TO OPTION TWO
+
+			}); //<<<<< END OF .CHANGE FUNCTION FOR PLAYER 2
 
 
 	}); //<<<<< END OF VAR RESPONSE TO GET JSON
