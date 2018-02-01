261ef0e04ff21498a71d671b27d25df71862d858
Ben Hall <Benjamin2442@hotmail.co.uk>
2018-02-01 13:52:49 +0000

    added template and got template to load but no data loading

--- js/dataLoad.js
+++ js/dataLoad.js
@@ -4,5 +4,13 @@ var dataResponse = $.getJSON('../data.json',function(){
 		var selectedPlayer = $('#second-choice1').val();
 		selectedPlayer = data.elements.filter( a => a.second_name === selectedPlayer);
 		console.log(selectedPlayer);
+
+			$('#content').empty();
+			var source   = $("#data-template").html();
+			var template = Handlebars.compile(source);
+			var html    = template(selectedPlayer);
+			console.log(html);
+			$('#content').append(html);
+
 	});
 });
