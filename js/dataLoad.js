ee2efbce402b4024d350131396900a5c75b48660
Ben Hall <Benjamin2442@hotmail.co.uk>
2018-02-01 15:39:22 +0000

    got the data to load and listed in #content

--- js/dataLoad.js
+++ js/dataLoad.js
@@ -3,12 +3,21 @@ var dataResponse = $.getJSON('../data.json',function(){
 	$('#second-choice1').change(function(){
 		var selectedPlayer = $('#second-choice1').val();
 		selectedPlayer = data.elements.filter( a => a.second_name === selectedPlayer);
-		console.log(selectedPlayer);
+		// console.log(selectedPlayer);
+
+
+		Handlebars.registerHelper("grandTotal", function(user_name) {
+		  var grandTotal = user_name/10;
+		  return grandTotal;
+		});
 
 			$('#content').empty();
 			var source   = $("#data-template").html();
 			var template = Handlebars.compile(source);
-			var html    = template(selectedPlayer);
+			var context = selectedPlayer[0];
+			console.log(context);
+			var html    = template(context);
+
 			console.log(html);
 			$('#content').append(html);
 
