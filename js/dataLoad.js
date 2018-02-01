ecfeaa3b0e08397970a8e4f88a3d4eebd0a3c7ec
Ben Hall <Benjamin2442@hotmail.co.uk>
2018-02-01 17:10:18 +0000

    got all data loading and badges added

--- js/dataLoad.js
+++ js/dataLoad.js
@@ -1,25 +1,35 @@
 var dataResponse = $.getJSON('../data.json',function(){
 	var data = response.responseJSON;
 	$('#second-choice1').change(function(){
-		var selectedPlayer = $('#second-choice1').val();
-		selectedPlayer = data.elements.filter( a => a.second_name === selectedPlayer);
+		var selectedPlayer1 = $('#second-choice1').val();
+		selectedPlayer1 = data.elements.filter( a => a.second_name === selectedPlayer1);
 		// console.log(selectedPlayer);
+		$('#content').empty();
+		var source   = $("#data-template").html();
+		var template = Handlebars.compile(source);
+		var context = selectedPlayer1[0];
+		console.log(context);
+		var html    = template(context);
+		console.log(html);
+		$('#content').append(html);
+	});
 
+	$('#second-choice2').change(function(){
+		var selectedPlayer2 = $('#second-choice2').val();
+		selectedPlayer2 = data.elements.filter( a => a.second_name === selectedPlayer2);
+		// console.log(selectedPlayer);
+		$('#content2').empty();
+		var source   = $("#data-template").html();
+		var template = Handlebars.compile(source);
+		var context = selectedPlayer2[0];
+		console.log(context);
+		var html    = template(context);
+		console.log(html);
+		$('#content2').append(html);
+	});
 
-		Handlebars.registerHelper("grandTotal", function(user_name) {
-		  var grandTotal = user_name/10;
-		  return grandTotal;
-		});
-
-			$('#content').empty();
-			var source   = $("#data-template").html();
-			var template = Handlebars.compile(source);
-			var context = selectedPlayer[0];
-			console.log(context);
-			var html    = template(context);
-
-			console.log(html);
-			$('#content').append(html);
-
+	Handlebars.registerHelper("grandTotal", function(user_name) {
+	  var grandTotal = user_name/10;  //<<<<<< FUNCTION TO DIVIDE COST/10
+	  return grandTotal;
 	});
 });
